1ffce4f52109fec81f2bae828043ea12c33d44e7
Schill93 <schill_93@hotmail.com>
2016-11-23 02:00:45 +0100

    Update

--- Roulette_mult.py
+++ Roulette_mult.py
@@ -2,17 +2,18 @@ from random import *
 
 list=[5,8,11,17,35]
 
-for i in range (0,20):
-    b=randint(1,20)
+for i in range (0,10):
+    b=randint(1,10)
     c=randint(0,len(list)-1)
-
-    print(b*list[c])
 
     a=int(input("%s * %s \n" % (b,list[c])))
 
+    if a==0:
+        break
 
     if a == b*list[c]:
         print('Success')
     else:
         print('Fail')
+        print("Correct answer is %s" % (b*list[c]))
 
