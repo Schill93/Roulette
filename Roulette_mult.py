7ed5b72a8d9f8bd5fe4c9b983da405df1835ccef
Sebastian Schill <schill_93@hotmail.com>
2017-01-26 22:12:25 +0100

    Added file again

---
+++ Roulette_mult.py
@@ -0,0 +1,63 @@
+import random
+from random import *
+
+
+<<<<<<< HEAD
+def count():
+
+    list = [5, 8, 11, 17, 35]
+    #list=[17,35]
+
+=======
+for i in range (0,10):
+    b=randint(1,10)
+    c=randint(0,len(list)-1)
+
+    a=int(input("%s * %s \n" % (b,list[c])))
+>>>>>>> origin/master
+
+    if a==0:
+        break
+
+
+    result=0
+    x=0
+
+    while x < len(list)-2:
+
+        b=randint(1,20)
+        c=randint(0,len(list)-1)
+
+        result=result+b*list[c]
+
+
+
+        print("%s * %s  " % ( list[c],b))
+
+        list.pop(c)
+
+
+    answer=int(input("Answer: \n"))
+
+
+
+
+    if answer==result:
+        print('Success')
+    else:
+        print('Fail')
+<<<<<<< HEAD
+        print(result)
+
+
+
+def main():
+    for i in range(0,10):
+        count()
+
+=======
+        print("Correct answer is %s" % (b*list[c]))
+>>>>>>> origin/master
+
+if __name__ == '__main__':
+    main()
