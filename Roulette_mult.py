9e0cde68810e98e7d8c9a0ce8ca3804ea2dc2be8
Sebastian Schill <schill_93@hotmail.com>
2017-01-26 22:09:18 +0100

    Improved how to pick numbers

--- Roulette_mult.py
+++ Roulette_mult.py
@@ -1,18 +1,49 @@
+import random
 from random import *
 
-list=[5,8,11,17,35]
 
-for i in range (0,20):
-    b=randint(1,20)
-    c=randint(0,len(list)-1)
+def count():
 
-    print(b*list[c])
-
-    a=int(input("%s * %s \n" % (b,list[c])))
+    list = [5, 8, 11, 17, 35]
+    #list=[17,35]
 
 
-    if a == b*list[c]:
+
+
+    result=0
+    x=0
+
+    while x < len(list)-2:
+
+        b=randint(1,20)
+        c=randint(0,len(list)-1)
+
+        result=result+b*list[c]
+
+
+
+        print("%s * %s  " % ( list[c],b))
+
+        list.pop(c)
+
+
+    answer=int(input("Answer: \n"))
+
+
+
+
+    if answer==result:
         print('Success')
     else:
         print('Fail')
+        print(result)
 
+
+
+def main():
+    for i in range(0,10):
+        count()
+
+
+if __name__ == '__main__':
+    main()
