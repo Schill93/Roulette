9df254323e29dd649b55167c414210810f1b7604
Schill93 <schill_93@hotmail.com>
2017-01-29 17:16:30 +0100

    Added timer functionality

--- Roulette_mult.py
+++ Roulette_mult.py
@@ -1,9 +1,15 @@
 import random
 from random import *
+import time
+import timeit
 
 
 
 def count():
+
+
+
+
 
     list = [5, 8, 11, 17, 35]
     #list=[17,35]
@@ -12,7 +18,7 @@ def count():
     x=0
     result=0
 
-    while x < (len(list)-3):
+    while x < (len(list)-2):
 
 
         b=randint(1,20)
@@ -36,16 +42,55 @@ def count():
 
     if answer==result:
         print('Success')
+        correct=True
+
     else:
         print('Fail')
+        correct=False
+
+
 
         print(result)
+
+    return correct
+
+
+def timer(start,end,nbr):
+
+    time=end-start
+
+    timePer=time/nbr
+
+    print('Total time: ' )
+    print(time-time%1)
+
+    print('Time per answer: ')
+
+    print(timePer-timePer%1)
+
 
 
 
 def main():
-    for i in range(0,10):
-        count()
+    nbr=2
+    correct = 0
+
+    start=timeit.default_timer()
+
+
+
+    for i in range(0,nbr):
+
+
+        if count() == True:
+            correct=correct+1
+
+
+
+
+    end=timeit.default_timer()
+
+    timer(start,end,nbr)
 
 
 
